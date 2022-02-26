e2fc6b865e5a7680739c3b2fc12bcaf60dd01b4f
Daniel Armbruster <danielgundam00@gmail.com>
2022-02-26 12:55:32 -0700

    commit tree 47a6c204cd83e6a783286073db66b519b7cd685a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array,string){
+    let outcome = array.filter( array => array.toLowerCase() === string.toLowerCase());
+    return outcome;
+   
+}
+
+
+function fuzzyMatch(array,string){
+    let outcome = array.filter( array => array.toLowerCase().indexOf(string.toLowerCase()) === 0);
+    return outcome;
+}
+
+function matchName(array,string){
+    let outcome = array.filter( array => array.name === string);
+    return outcome
+}
